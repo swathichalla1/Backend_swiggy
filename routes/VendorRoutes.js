e64cc7682b42ba292b8938c55922c511904ca684
swathi <swathichalla0@gmail.com>
2024-04-21 00:33:57 +0530

    corrected error

--- routes/VendorRoutes.js
+++ routes/VendorRoutes.js
@@ -1,4 +1,4 @@
-const vendorController = require("../controllers/vendorController");
+const vendorController = require("../controllers/VendorController");
 const express = require("express");
 
 const router = express.Router();
